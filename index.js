371349217e0457be3e9ad845b6f3dcfd2934df7b
Fuad Ibrahimov <fuad.ibrahimov@live.com>
2019-12-13 15:14:09 +0300

    Added instance of ScreenManager to Prompt

--- index.js
+++ index.js
@@ -1,48 +1,57 @@
 var _ = require("lodash");
 var util = require("util");
 var inquirer = require("inquirer");
-
+var ScreenManager = require('inquirer/lib/utils/screen-manager');
 var Base = require("inquirer/lib/prompts/base");
 
 module.exports = Prompt;
 
 function Prompt(question, rl, answers) {
-    // Set defaults prompt options
-    this.opt = _.defaults(_.clone(question), {
-        validate: function () { return true; },
-        filter: function (val) { return val; },
-        when: function () { return true; }
-    });
-    this.responses = [];
-    return this;
-}
-util.inherits( Prompt, Base );
+  // Set defaults prompt options
+  this.screen = new ScreenManager(rl);
 
-Prompt.prototype.askForLoop = function() {
-    var ui = inquirer.prompt({
-        default: true,
-        type:'confirm',
-        name: 'loop',
-        message: this.opt.message || 'Would you like to loop ?'
-    }).then(function (result) {
-        if(result.loop) {
-            this.askNestedQuestion();
-        } else {
-            this.done( this.responses );
-        }
-    }.bind(this));
+  this.opt = _.defaults(_.clone(question), {
+    validate: function () {
+      return true;
+    },
+    filter: function (val) {
+      return val;
+    },
+    when: function () {
+      return true;
+    }
+  });
+  this.responses = [];
+  return this;
 }
 
-Prompt.prototype.askNestedQuestion = function() {
-    inquirer.prompt(this.opt.prompts).then(function (result) {
-        this.responses.push(result);
-        this.askForLoop();
-    }.bind(this));
+util.inherits(Prompt, Base);
+
+Prompt.prototype.askForLoop = function () {
+  var ui = inquirer.prompt({
+    default: true,
+    type: 'confirm',
+    name: 'loop',
+    message: this.opt.message || 'Would you like to loop ?'
+  }).then(function (result) {
+    if (result.loop) {
+      this.askNestedQuestion();
+    } else {
+      this.done(this.responses);
+    }
+  }.bind(this));
 }
 
-
-Prompt.prototype._run = function( cb ) {
-    this.done = cb;
+Prompt.prototype.askNestedQuestion = function () {
+  inquirer.prompt(this.opt.prompts).then(function (result) {
+    this.responses.push(result);
     this.askForLoop();
-    return this;
+  }.bind(this));
+}
+
+
+Prompt.prototype._run = function (cb) {
+  this.done = cb;
+  this.askForLoop();
+  return this;
 };
